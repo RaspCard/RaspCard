cadfd438c188a46905189aae27dbb1f52afc8481
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-08-31 17:35:21 +0200

    add login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import type { JsonResponse } from '$lib/types';
+    let userUUID: string = '';
+
+    async function handleLogin() {
+        console.log("ok");
+
+        const response = await fetch('/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                uuid: userUUID
+            })
+        });
+
+        const data: JsonResponse  = await response.json();
+
+        if(data.success) {
+            window.location.href = '/';
+        }
+    }
+</script>
+
+
+
+<input bind:value={userUUID} type="text">
+<button on:click="{handleLogin}">Register</button>
