b1f159622af8120fd2db7a56d6d1b04506d86cdb
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-08-31 17:35:34 +0200

    add testing route

---
+++ src/routes/protected/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import { writable } from "svelte/store";
+    const x = writable(0);
+</script>
+
+<h1>Protected route</h1>
---
+++ src/routes/protected/+page.ts
@@ -0,0 +1,11 @@
+import type { PageLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
+
+
+export const load: PageLoad = async({ parent }) => {
+    const { user } = await parent();
+
+    if (!user) {
+        throw redirect(302, '/login');
+    }
+}
