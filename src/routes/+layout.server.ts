53405a8f5506749d53b42181e6cbcb7ac3a84235
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-08-31 17:33:43 +0200

    edit layout and add custom load

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,17 @@
+import { db } from "$lib/database";
+import type { LayoutServerLoad } from './$types';
+
+
+export const csr = false;
+
+
+export const load: LayoutServerLoad = async({ request }) => {
+    const cookies = request.headers.get('cookie');
+    const session = cookies?.split('; ').find(row => row.startsWith('session='))?.split('=')[1];
+
+    const user = session ? await db.user.findUnique({ where: { id: session } }) : null;
+
+    return {
+        user
+    };
+}
