845a6736c18d3998ef46f691984a86f790ac6a3e
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-08-31 17:33:53 +0200

    ionic testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,57 +1,20 @@
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+    let myInput: string = "";
+
+    function test() {
+        alert("test");
+    }
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<ion-app>
+    <ion-header translucent>
+      <ion-toolbar>
+          <ion-title>Button</ion-title>
+      </ion-toolbar>
+     </ion-header>
+    <ion-content fullscreen>
+        <ion-label>Default Label</ion-label>
+        <ion-input placeholder="placeholder" />
+        <ion-button on:click={test}>Register</ion-button>
+    </ion-content>
+</ion-app>
