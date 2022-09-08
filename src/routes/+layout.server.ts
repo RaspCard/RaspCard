578397052a5f47fbe5a525cdc5e0a83e782bc41b
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-09-08 15:54:30 +0200

    minor fix on layout

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,16 +2,11 @@ import { db } from "$lib/database";
 import type { LayoutServerLoad } from './$types';
 
 
-export const csr = false;
-
-
 export const load: LayoutServerLoad = async({ request }) => {
     const cookies = request.headers.get('cookie');
     const session = cookies?.split('; ').find(row => row.startsWith('session='))?.split('=')[1];
 
     const user = session ? await db.user.findUnique({ where: { id: session } }) : null;
 
-    return {
-        user
-    };
+    return { user };
 }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,7 @@
+<script>
+  import "../app.postcss";
+</script>
+
 <main>
-    <slot></slot>
+  <slot />
 </main>
