5f1bb409ae4689d450fb13e1ff47140d4a58892f
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-09-12 16:13:18 +0200

    move and create components to lib for more practical use

---
+++ src/lib/components/Head.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import Logo from './Logo.svelte';
+
+    export let establishment: string;
+    export let seller: string;
+</script>
+
+<div class="logo">
+    <Logo/>
+    <div>
+        <h1>{establishment}</h1>
+        <p>{seller}</p>
+    </div>
+</div>
+
+<style>
+    .logo {
+        height: 3rem; /* temp 48px then fit-content */
+
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0px;
+    }
+</style>
