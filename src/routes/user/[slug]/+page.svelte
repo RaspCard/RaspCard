9be22d079fece4c5e8a312e73e09cebf9322edb3
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-09-12 16:13:58 +0200

    upload new routes

---
+++ src/routes/user/[slug]/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import { Button, Modal } from 'flowbite-svelte';
+    import * as Icon from 'svelte-heros-v2'
+    import Head from '$lib/components/Head.svelte';
+
+    let establishment: string = "STREGATTO";
+    let seller: string = "SELLER #1";
+
+    let deleteModal: boolean = false;
+</script>
+
+<div class="container-raspcard base-dimention">
+    <div>
+        <Head establishment={establishment} seller={seller}/>
+    </div>
+    <Button gradient color="blue"><Icon.CurrencyEuro/> New Transaction</Button>
+    <Button color="blue"><Icon.Backward/> Rollback</Button>
+    <Button on:click={() => deleteModal = true} gradient color="red"><Icon.Trash/> Delete User</Button>
+</div>
+
+
+<!-- Delete modal -->
+<Modal bind:open={deleteModal}>
+    <div class="text-center">
+        <Icon.ExclamationTriangle class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200"/>
+        <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this user?</h3>
+        <Button color="red" class="mr-2">Yes, I'm sure</Button>
+        <Button color='alternative'>No, cancel</Button>
+    </div>
+</Modal>
+
+<style>
+    .container-raspcard {
+        
+    }
+
+    .container-table {
+        
+    }
+</style>
---
+++ src/routes/users/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import { Table, TableHead, TableHeadCell, TableBody, TableBodyRow, TableBodyCell } from 'flowbite-svelte';
+    import Head from '$lib/components/Head.svelte';
+
+    let establishment: string = "STREGATTO";
+    let seller: string = "SELLER #1";
+</script>
+
+<div class="container-raspcard base-dimention">
+    <div>
+        <Head establishment={establishment} seller={seller}/>
+    </div>
+    <div class="container-table">
+        <Table shadow>
+            <TableHead>
+                <TableHeadCell>ID</TableHeadCell>
+                <TableHeadCell>name</TableHeadCell>
+                <TableHeadCell>surname</TableHeadCell>
+                <TableHeadCell>phone number</TableHeadCell>
+                <TableHeadCell>
+                    <span class="sr-only"> Edit </span>
+                </TableHeadCell>
+            </TableHead>
+            <TableBody class="divide-y">
+                <TableBodyRow>
+                    <TableBodyCell>123456789</TableBodyCell>
+                    <TableBodyCell>Mario</TableBodyCell>
+                    <TableBodyCell>Mario</TableBodyCell>
+                    <TableBodyCell>+39 392 9436267</TableBodyCell>
+                    <TableBodyCell>
+                        <a href="/user/mario" class="font-medium text-blue-600 hover:underline dark:text-blue-500">
+                            Edit
+                        </a>
+                    </TableBodyCell>
+                </TableBodyRow>
+            </TableBody>
+        </Table>
+    </div>
+</div>
+
+<style>
+    .container-raspcard {
+
+    }
+
+    .container-table {
+        
+    }
+</style>
