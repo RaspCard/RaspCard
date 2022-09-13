ce4f2509447784c8e0e95382c39f06131aacc67e
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-09-13 16:47:46 +0200

    complete dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -9,26 +9,30 @@
 
 </script>
 
-<div class="container-raspcard base-dimention">
+
+<div class="container-raspcard b-d">
     <div class="container-data"> <!-- column 1 -->
         <Head establishment={establishment} seller={seller}/>
         <div class="text"> <!-- row 2 -->
             <h1>WELCOME</h1>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus eligendi similique beatae quam ratione sit at optio reprehenderit voluptatem et vitae dolores repudiandae quo, in sapiente officiis nam iure necessitatibus.
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus eligendi similique beatae quam ratione sit at optio reprehenderit voluptatem et vitae dolores repudiandae quo, in sapiente officiis nam iure necessitatibus.
+            </p>
         </div>
     </div>
     <div class="button-group"> <!-- column 2 -->
         <div class="button-list">
-            <Button on:click={() => goto("/scanner")} gradient color="blue" style="width: 100%; height: 70px;"><Icon.CreditCard/> Scan</Button>
-            <Button on:click={() => goto("/users")} gradient color="blue" style="width: 100%; height: 70px;"><Icon.Users/> All Users</Button>
-            <Button on:click={() => goto("/create")} gradient color="blue" style="width: 100%; height: 70px;"><Icon.Plus/> Create Card</Button>
-            <Button on:click={() => goto("/migrate")} gradient color="blue" style="width: 100%; height: 70px;"><Icon.CloudArrowUp/> Migrate?</Button>
+            <Button on:click={() => goto("/scanner")} gradient color="blue" class="h-20 w-full"><Icon.CreditCard class="mr-2 -ml-1 w-7 h-7"/> Scan</Button>
+            <Button on:click={() => goto("/users")} gradient color="blue" class="h-20 w-full"><Icon.Users class="mr-2 -ml-1 w-7 h-7"/> All Users</Button>
+            <Button on:click={() => goto("/create")} gradient color="blue" class="h-20 w-full"><Icon.Plus class="mr-2 -ml-1 w-7 h-7"/> Create Card</Button>
+            <Button on:click={() => goto("/migrate")} gradient color="blue" class="h-20 w-full"><Icon.CloudArrowUp class="mr-2 -ml-1 w-7 h-7"/> Migrate?</Button>
         </div>
         <div class="button-list">
-            <Button on:click={() => goto("/logout")} gradient color="blue" style="width: 100%; heigt: 40px;"><Icon.ArrowLeftOnRectangle/> Logout</Button>
+            <Button on:click={() => goto("/logout")} gradient color="blue" class="h-15 w-full"><Icon.ArrowLeftOnRectangle class="mr-2 -ml-1 w-7 h-7"/> Logout</Button>
         </div>
     </div>
 </div>
+
 
 <style>
     .container-raspcard {
@@ -46,7 +50,11 @@
     }
 
     .text {
-
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 0px;
+        gap: 10px;
     }
 
     .button-group {
@@ -66,9 +74,9 @@
         padding: 0px 16px;
         gap: 25px;
     }
-
+/* 
     .button-list > * {
         width: 100%;
         height: 50px;
-    }
+    } */
 </style>
