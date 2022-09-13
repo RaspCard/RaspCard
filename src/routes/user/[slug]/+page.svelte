7cd1efef4af7eb41dc84c93f8d3fd3dc2bb5d1eb
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-09-13 22:40:42 +0200

    update all cards and add new modal for transactions

--- src/routes/user/[slug]/+page.svelte
+++ src/routes/user/[slug]/+page.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
-    import { Button, Modal, Card } from 'flowbite-svelte';
+    import { Button, Modal, Listgroup, Input, Label, Radio } from 'flowbite-svelte';
     import { goto } from '$app/navigation';
     import * as Icon from 'svelte-heros-v2'
     import Head from '$lib/components/Head.svelte';
     import BaseCard from '$lib/components/BaseCard.svelte';
+    import ListItem from '$lib/components/ListItem.svelte';
 
     let establishment: string = "STREGATTO";
     let seller: string = "SELLER #1";
 
     let user;
-
+    let list = [
+        { name: "Made in Abyss 11", status: "to be shipped soon" },
+        { name: "Berserk 33", status: "shipped" },
+        { name: "Grand Blue jp 1", status: "delivered" },
+        { name: "Grand Blue jp 1", status: "delivered" },
+        { name: "Grand Blue jp 1", status: "delivered" },
+        { name: "Grand Blue jp 1", status: "delivered" },
+    ];
 
     let deleteModal: boolean = false;
+    let transactionModal: boolean = false;
+
+    let choice: string = "normal";
 </script>
 
 
@@ -23,73 +34,91 @@
             <Button on:click={() => goto("/dashboard")} gradient color="blue" class="w-56"><Icon.RectangleGroup class="mr-2 -ml-1 w-7 h-7"/>Dashboard</Button>
         </div>
     </div>
-    <div class="container-content">
+    <div class="container-content mt-6">
         <div>
             <div> <!-- Top card -->
                 <BaseCard title="Profile Data">
-                    <ul class="container-profile-data">
-                        <li class="py-2 px-4 w-full text-sm font-medium">
-                            <div class="flex items-center space-x-4">
-                                <div class="flex-1 space-y-1 font-medium">
-                                    <div class="text-base text-gray-500">Name</div>
-                                    <div class="text-sm">{null}</div>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="py-2 px-4 w-full text-sm font-medium border-t-2">
-                            <div class="flex items-center space-x-4">
-                                <div class="flex-1 space-y-1 font-medium">
-                                    <div class="text-base text-gray-500">Surname</div>
-                                    <div class="text-sm">{null}</div>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="py-2 px-4 w-full text-sm font-medium border-t-2">
-                            <div class="flex items-center space-x-4">
-                                <div class="flex-1 space-y-1 font-medium">
-                                    <div class="text-base text-gray-500">PhoneNumber</div>
-                                    <div class="text-sm">{null}</div>
-                                </div>
-                            </div>
-                        </li>
+                    <ul>
+                        <ListItem fieldName={"Name"} fieldValue={null}/>
+                        <ListItem fieldName={"Surname"} fieldValue={null} border={true}/>
+                        <ListItem fieldName={"PhoneNumber"} fieldValue={null} border={true}/>
                     </ul>
                 </BaseCard>
             </div>
             <div> <!-- Bottom card -->
                 <BaseCard title="Balance">
-
+                    <div class="w-full flex items-center text-lg">
+                        <p>{123}€</p>
+                    </div>
                 </BaseCard>
             </div>
         </div>
         <div> <!-- Last Transaction card -->
             <BaseCard title="Last Transaction">
-
+                <ul>
+                    <ListItem fieldName={"Transaction"} fieldValue={null}/>
+                    <ListItem fieldName={"Date"} fieldValue={null} border={true}/>
+                    <ListItem fieldName={"Field"} fieldValue={null} border={true}/>
+                    <ListItem fieldName={"Field"} fieldValue={null} border={true}/>
+                </ul>
             </BaseCard>
         </div>
         <div> <!-- Func card -->
             <BaseCard title="Field">
-
+                <ul>
+                    <ListItem fieldName={"Field"} fieldValue={null}/>
+                    <ListItem fieldName={"Field"} fieldValue={null} border={true}/>
+                </ul>
+                <div class="text-lg text-gray-500 border-t-2 my-2">{"Field"}</div>
+                <Listgroup items={list} let:item class="border-0 h-52 overflow-scroll">
+                    <div class="flex items-center space-x-4">
+                        <div class="flex-1 space-y-1 font-medium">
+                          <div>{item.name}</div>
+                          <div class="text-sm text-gray-500">{item.status}</div>
+                        </div>
+                        <Button gradient color="green" class="h-8">Details</Button>
+                    </div>
+                </Listgroup>
             </BaseCard>
         </div>
     </div>
-    <div class="m-4 bottom-buttons-container">
-        <Button on:click={() => deleteModal = true} gradient color="red"><Icon.Trash/> Delete User</Button>
+    <div class="m-4 mb-10 bottom-buttons-container">
+        <Button on:click={() => deleteModal = true} gradient color="red" class="w-56"><Icon.Trash/> Delete User</Button>
         <div class="container-buttons">
             <Button gradient color="blue" class="w-56"><Icon.Backward/>Rollback</Button>
-            <Button gradient color="blue" class="w-56"><Icon.CurrencyEuro/>New Transaction</Button>    
+            <Button on:click={() => transactionModal = true} gradient color="blue" class="w-56"><Icon.CurrencyEuro/>New Transaction</Button>    
         </div>
     </div>
 </div>
 
 
 <!-- Delete modal -->
-<Modal bind:open={deleteModal}>
+<Modal bind:open={deleteModal} size="sm">
     <div class="text-center">
         <Icon.ExclamationTriangle class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200"/>
         <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this user?</h3>
         <Button color="red" class="mr-2">Yes, I'm sure</Button>
         <Button color='alternative'>No, cancel</Button>
     </div>
+</Modal>
+
+
+<!-- Transaction modal -->
+<Modal bind:open={transactionModal} size="xs">
+    <form class="flex flex-col space-y-6">
+		<h3 class="text-xl font-medium text-gray-900 dark:text-white p-0">New Transaction</h3>
+		<div class="container-transaction-conent">
+            <Label class="space-y-2">
+                <span>Amount</span>
+                <Input type="number" name="amount" placeholder="0" required class="w-24"/>
+		    </Label>
+            <div class="flex flex-col gap-1">
+                <Radio bind:group={choice} value="comics">Comics</Radio>
+                <Radio bind:group={choice} value="normal">Normal</Radio>
+            </div>
+        </div>
+		<Button type="submit" class="w-full">Confirm</Button>
+	</form>
 </Modal>
 
 
@@ -115,12 +144,6 @@
         gap: 1rem;
     }
 
-    .container-profile-data {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-    }
-
     .container-content {
         display: grid;
         grid-template-columns: 30vw 30vw 40vw;
@@ -128,6 +151,13 @@
     }
 
     .bottom-buttons-container {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .container-transaction-conent {
         display: flex;
         flex-direction: row;
         align-items: center;
