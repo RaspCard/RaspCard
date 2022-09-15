b21a0793aef784413a351bb2bd22be84905e0f2f
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-09-15 17:49:05 +0200

    add dispatcher for the scanner

--- src/lib/components/Scanner.svelte
+++ src/lib/components/Scanner.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+    import QrScanner from 'qr-scanner';
+    const dispatch = createEventDispatcher();
 
+    // const qrScanner = new QrScanner(
+    //     videoElem,
+    //     result => dispatch('scan-success', { result: result }),
+    //     { /* your options or returnDetailedScanResult: true if you're not specifying any other options */ },
+    // );
 </script>
 
 
