8bde44c538528e6f52105cb395888095506a6582
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-09-17 14:33:51 +0200

    create new component for fast head with default buttons

---
+++ src/lib/components/HeadWithButtons.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import { Button } from 'flowbite-svelte';
+    import { goto } from '$app/navigation';
+    import * as Icon from 'svelte-heros-v2';
+    import Head from '$lib/components/Head.svelte';
+    
+    export let establishment: string;
+    export let seller: string;
+</script>
+
+
+<div class="m-4 head-container">
+    <Head establishment={establishment} seller={seller}/>
+    <div class="container-buttons">
+        <Button on:click={() => goto("/scanner")} gradient color="blue" class="w-56"><Icon.CreditCard class="mr-2 -ml-1 w-7 h-7"/>Scan</Button>
+        <Button on:click={() => goto("/dashboard")} gradient color="blue" class="w-56"><Icon.RectangleGroup class="mr-2 -ml-1 w-7 h-7"/>Dashboard</Button>
+    </div>
+</div>
+
+
+<style>
+    .head-container {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .container-buttons {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        gap: 1rem;
+    }
+</style>
