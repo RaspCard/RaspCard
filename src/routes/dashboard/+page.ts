f8c6fd60245cb35891247154057a17c76610ecb3
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-09-30 21:00:13 +0200

    upload check for session on load for access page

---
+++ src/routes/dashboard/+page.ts
@@ -0,0 +1,13 @@
+import { redirect } from "@sveltejs/kit";
+import type { PageLoad } from "./$types";
+
+
+export const load: PageLoad = async ({parent}) => {
+    const { currentAdmin } = await parent();
+
+    if (!currentAdmin) {
+        throw redirect(307, '/login');
+    }
+
+    return { };
+}
