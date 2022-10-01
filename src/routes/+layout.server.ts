591ff42de7131b93076f2531c6e0ef14d4189428
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-10-01 18:14:36 +0200

    remove boilerplate code

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,18 +1,6 @@
-import { db } from "$lib/server/database";
-import * as cookie from "cookie";
 import type { LayoutServerLoad } from './$types';
 
 
-export const load: LayoutServerLoad = async({ request }) => {
-    const cookies = cookie.parse(request.headers.get('cookie') || '');
-
-    if(!cookies.session) {
-        return {
-            currentAdmin: null
-        };
-    }
-
-    const user = await db.admin.findUnique({ where: { id: cookies.session }, include: { establishment: true } });
-
-    return { currentAdmin: user };
+export const load: LayoutServerLoad = async({ locals }) => {
+    return { currentAdmin: locals.currentAdmin };
 }
