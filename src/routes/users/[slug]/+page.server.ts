03be815622358abf275199063a515070ff797b86
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-10-08 11:56:17 +0200

    add transaction backend

--- src/routes/users/[slug]/+page.server.ts
+++ src/routes/users/[slug]/+page.server.ts
@@ -22,18 +22,41 @@ export const load: PageServerLoad = async({ locals, params }) => {
 }
 
 export const actions: Actions = {
-    async balance({locals, params}) {
+    async balance({locals, params, request}) {
         if(!locals.currentAdmin) {
             return invalid(401);
         }
 
-        
+        const data = await request.formData();
+        const amount = parseInt(data.get('amount')?.toString() || '');
+
+        if(isNaN(amount)) {
+            return invalid(400, {success: false, message: 'Saldo invalido'});
+        }
+
+        try {
+            await db.user.updateMany({
+                where: {
+                    id: params.slug,
+                    establishmentId: locals.currentAdmin.establishmentId
+                }, data: {
+                    balance: {
+                        increment: amount
+                    }
+                }
+            });
+        } catch {
+            return invalid(400, {success: false, message: 'Qualcosa è andato storto nella richiesta'});
+        }
+
+        return {success: true, message: '', balance: amount};
     },
 
     async rollback({locals, params}) {
         if(!locals.currentAdmin) {
             return invalid(401);
         }
+
     },
 
     async delete({ locals, params }) {
