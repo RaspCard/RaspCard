56db9d6df7308a501690c950e39ee69712154446
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-10-12 22:16:57 +0200

    change redirect to send to index

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -4,6 +4,6 @@ import { redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async({ locals }) => {
     if(!locals.currentAdmin) {
-        throw redirect(302, '/login');
+        throw redirect(302, '/');
     }
 }
