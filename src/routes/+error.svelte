4384c3b3724f6b6da3172cdff5ba26343c855a57
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-10-22 17:52:21 +0200

    edit current error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-
+    import { Button } from "flowbite-svelte";
+    import { goto } from "$app/navigation";
 </script>
 
 <h1>
     Error
 </h1>
+<Button on:click={() => goto("/")}>Vai alla Home</Button>
