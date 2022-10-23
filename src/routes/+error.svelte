aaceb28f0b419b5ee0eaa43a80a497d464f439bc
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-10-23 20:39:57 +0200

    add button to home

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
     import { Button } from "flowbite-svelte";
-    import { goto } from "$app/navigation";
 </script>
 
 <h1>
     Error
 </h1>
-<Button on:click={() => goto("/")}>Vai alla Home</Button>
+<Button href="/">Vai alla Home</Button>
