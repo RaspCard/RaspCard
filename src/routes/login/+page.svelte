2933828f3b3e178d1f591dc64e78c1e999648725
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-11-08 00:27:33 +0100

    test disable enter on RFID reader

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-
+    import Scanner from '$lib/components/Scanner.svelte';
+    let input: HTMLInputElement;
 </script>
 
+<Scanner on:scan={(event) => input.value=event.detail.id}/>
 
 <form method="POST">
-    <input name="uuid" type="text">
+    <input bind:this={input} name="uuid" type="text" on:keypress={(event) => event.key != 'Enter'}>
     <button type="submit">Login</button>
 </form>
