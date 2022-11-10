d8d27c91232331edd898de38ee016e970cce3c71
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-11-10 17:10:14 +0100

    remove useless style and update home graphics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,56 +1,24 @@
 <script lang="ts">
+    import { P, Heading, Card } from 'flowbite-svelte';
     import { goto } from '$app/navigation';
     import Scanner from "$lib/components/Scanner.svelte";
+
+    
 </script>
 
+
 <Scanner on:scan={(event) => goto(`/users/${event.detail.id}`)}/>
-<div class="container-raspcard">
-    <div class="m-4 container-data">
-        <div class="text">
-            <h1>WELCOME</h1>
-            <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus eligendi similique beatae quam ratione sit at optio reprehenderit voluptatem et vitae dolores repudiandae quo, in sapiente officiis nam iure necessitatibus.
-            </p>
+<div class="m-4 flex flex-col items-start gap-4">
+    <Heading tag="h1">Benvenuto</Heading>
+    <P>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus eligendi similique beatae quam ratione sit at optio reprehenderit voluptatem et vitae dolores repudiandae quo, in sapiente officiis nam iure necessitatibus.
+    </P>
+    <Card class="p-4">
+        <div class="flex flex-col items-center">
+            <Heading tag="h5" class="mb-2">Utenti Attivi</Heading>
+            <div class="flex space-x-3">
+                <P size="lg" weight="semibold">{"123"}</P>
+            </div>
         </div>
-    </div>
+      </Card>
 </div>
-
-
-<style>
-    .container-raspcard {
-        display: grid;
-        grid-template-columns: 60vw 40vw;
-        grid-template-rows: 1fr;
-    }
-
-    .container-data {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        gap: 20px;
-    }
-
-    .text {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        padding: 0px;
-        gap: 10px;
-    }
-
-    .button-group {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        align-items: center;
-    }
-
-    .button-list {
-        width: 100%;
-
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 25px;
-    }
-</style>
