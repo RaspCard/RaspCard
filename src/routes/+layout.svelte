0d816fb195583aadfa3203d68f1a22d04f0355ff
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-11-14 22:45:25 +0100

    add div for center login and error page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,9 +9,11 @@
 </svelte:head>
 
 <main class="b-d">
-  {#if $page.url.pathname === '/login'}
-    <slot />
-  {:else}
+  {#if $page.url.pathname === '/login' || $page.status !== 200}
+    <div class="h-full w-full flex justify-center items-center">
+      <slot />
+    </div>
+  {:else if $page.status === 200}
     <Sidebar establishment={$page.data.currentAdmin.establishmentName} seller={$page.data.currentAdmin.name} activeURL={$page.url.pathname}>
       <slot />
     </Sidebar>
