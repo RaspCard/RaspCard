0f72c5249117948f3ce5ad2222de18e5f96439cc
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-11-14 22:44:30 +0100

    update error page graphics

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
-    import { Button } from "flowbite-svelte";
+    import { Button, P } from "flowbite-svelte";
+    import { page } from "$app/stores";
 </script>
 
-<h1>
-    Error
-</h1>
-<Button href="/">Vai alla Home</Button>
+<div class="flex flex-col gap-2 max-w-[20rem]">
+    <div class="flex flex-row">
+        <P weight="thin" size="5xl" class="mr-2">
+            {$page.status}
+        </P>
+        <P weight="thin" size="2xl" class="flex items-center border-l-[1px] border-l-gray-400 pl-2">
+            {#if $page.status === 404}
+                La pagina/utente che stai cercando non esiste!
+            {:else if $page.status === 500}
+                C'è stato un errore nel server.
+            {:else}
+                C'è stato un errore.
+            {/if}
+        </P>
+    </div>
+    <Button outline color="dark" href="/">Torna indietro!</Button>
+</div>
