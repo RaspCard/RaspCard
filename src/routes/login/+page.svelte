1ec0d666f857e491488641e880aa7e1755d16ace
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-11-15 10:19:32 +0100

    update login form

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,21 +1,33 @@
 <script lang="ts">
     import { Card, Button, Label, Input, Heading } from "flowbite-svelte";
     import Scanner from '$lib/components/Scanner.svelte';
+    import { applyAction, enhance } from "$app/forms";
+    import { invalidateAll } from "$app/navigation";
     let input: string = "";
 </script>
 
 <Scanner on:scan={(event) => input=event.detail.id}/>
-<Card class="min-w-[30rem]">
-    <form class="flex flex-col space-y-6" method="POST">
+<Card class="sm:max-w-[20rem] lg:min-w-[30rem]">
+    <form 
+        class="flex flex-col space-y-6"
+        method="POST"
+        use:enhance={() => {
+            return async ({ result }) => {
+                invalidateAll();
+                await applyAction(result);
+            }
+        }}
+        on:keypress={(event) => (event.key == "Enter") ? event.preventDefault() : null}
+    >
         <Heading tag="h3" class="p-0">Accedi a RaspCard</Heading>
         <Label class="space-y-2">
-            <span>Email</span>
-            <Input name="uuid" type="text" placeholder="il codice della tua carta" required bind:value={input} on:keypress={(event) => event.key != 'Enter'}/>
+            <span>Codice della carta</span>
+            <Input name="uuid" type="text" placeholder="codice della carta" required bind:value={input}/>
         </Label>
         <Label class="space-y-2">
             <span>PIN</span>
-            <Input type="password" name="password" placeholder="•••••" required on:keypress={(event) => event.key != 'Enter'}/>
+            <Input type="password" name="password" placeholder="•••••" required/>
         </Label>
-        <Button type="submit" class="w-full">Login to your account</Button>
+        <Button type="submit" outline color="blue" class="w-full">Login to your account</Button>
     </form>
 </Card>
