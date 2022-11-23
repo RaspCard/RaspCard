ba7f067ddcd5787f0685f58b4a7370915866740d
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-11-23 21:55:37 +0100

    update for phone number and rollback

--- src/routes/create/+page.server.ts
+++ src/routes/create/+page.server.ts
@@ -36,13 +36,13 @@ export const actions: Actions = {
             return invalid(400, {success: false, message: "L'utente esiste già"});
         }
 
-        const parsedPhoneNumber = parseInt(data.phoneNumber || '');
         const parsedDeposit = parseFloat(data.amount || '');
 
-        if(!isNaN(parsedPhoneNumber)) {
+        if(data.phoneNumber && data.phoneNumber !== '') {
             const user = await db.user.findFirst({
                 where: {
-                    phoneNumber: parsedPhoneNumber
+                    phoneNumber: data.phoneNumber,
+                    establishmentId: locals.currentAdmin.establishmentId
                 }
             });
 
@@ -50,19 +50,21 @@ export const actions: Actions = {
                 return invalid(400, {success: false, message: 'Numero di telefono già in uso'});
             }
         }
-    
-    
+
         await db.user.create({
             data: {
                 cardId: data.cardId,
                 name: data.name || null,
                 surname: data.surname || null,
-                phoneNumber: isNaN(parsedPhoneNumber) ? null : parsedPhoneNumber,
+                phoneNumber: data.phoneNumber || null,
                 balance: isNaN(parsedDeposit) ? 0 : parsedDeposit,
-                establishmentId: locals.currentAdmin.establishmentId
+                establishmentId: locals.currentAdmin.establishmentId,
+                rollback: {
+                    create: {}
+                }
             }
         });
-    
+
         return {success: true, message: 'Utente creato con successo' };
     }
 }
