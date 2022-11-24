9bee1e08163b4aba3d14d3a291c348218bcea96e
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-11-24 11:53:08 +0100

    update listitem to display a badge

--- src/lib/components/ListItem.svelte
+++ src/lib/components/ListItem.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+    import { Badge } from "flowbite-svelte";
+
     export let fieldName: string;
     export let fieldValue: any;
+    export let currency: string | undefined = "";
     export let border: boolean = false;
+
+    function isNumber(value: any) {
+        return typeof value === "number";
+    }
 </script>
 
 
@@ -9,7 +16,14 @@
     <div class="flex items-center space-x-4">
         <div class="flex-1 space-y-1 font-medium">
             <div class="text-base text-gray-500">{fieldName}</div>
-            <div class="text-sm">{fieldValue}</div>
+            <div class="text-sm">
+                {`${fieldValue}${currency !== undefined ? currency : ""}`}
+                {#if isNumber(fieldValue) && fieldValue < 0}
+                    <Badge class="ml-2" large={true} color="red">PAGAMENTO</Badge>
+                {:else if isNumber(fieldValue) && fieldValue > 0}
+                    <Badge class="ml-2" large={true} color="green">DEPOSITO</Badge>
+                {/if}
+            </div>
         </div>
     </div>
 </li>
