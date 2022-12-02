acd74fd73694e773d32f734d2b04cea325111e12
Alessandro <59316554+logeFox@users.noreply.github.com>
2022-12-02 17:34:52 +0100

    update welcome text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { P, Heading, Card } from 'flowbite-svelte';
+    import { P, Heading, Card, Span } from 'flowbite-svelte';
     import { goto } from '$app/navigation';
     import Scanner from "$lib/components/Scanner.svelte";
     import type { PageData } from './$types';
@@ -7,11 +7,12 @@
     export let data: PageData;
 </script>
 
-<Scanner on:scan={(event) => goto(`/users/${event.detail.id}`)}/>
+<Scanner on:scan={e => goto(`/users/${e.detail.id}`)}/>
 <div class="m-4 flex flex-col items-start gap-4">
     <Heading tag="h1">Benvenuto</Heading>
-    <P>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus eligendi similique beatae quam ratione sit at optio reprehenderit voluptatem et vitae dolores repudiandae quo, in sapiente officiis nam iure necessitatibus.
+    <P size="lg" italic space="wide" whitespace="preline">
+        Il Nuovo sistema RaspCard con l'uso di un <Span class="font-semibold">dispositivo RFID</Span> permette di accedere al sistema con un semplice tocco.<br/>
+        <Span class="font-semibold">Online</Span> e <Span class="font-semibold">centralizzato</Span> in un unico punto, RaspCard è molto più veloce e semplice da usare.
     </P>
     <Card class="p-4">
         <div class="flex flex-col items-center">
