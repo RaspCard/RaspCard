1b4de54af945b501536cdceb4036bd4ab4953f1e
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-02-21 12:42:45 +0100

    update namespace standard for locals data

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,17 +1,19 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-// and what to do when importing types
-declare namespace App {
-	interface Locals {
-		currentAdmin: {
-			id: string;
-			name: string;
-			establishmentId: number;
-			establishmentName: string;
+declare global {
+	namespace App {
+		// interface Error {}
+		interface Locals {
+			currentAdmin: {
+				id: string;
+				name: string;
+				establishmentId: number;
+				establishmentName: string;
+			}
 		}
+		// interface PageData {}
+		// interface Platform {}
 	}
-	
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
 }
+
+export {};
