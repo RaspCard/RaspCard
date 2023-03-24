98c7746fb4b45c6abed47308a71ee2fd2a78ee4e
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-03-24 18:23:28 +0100

    update layout with new navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,20 @@
 <script>
   import "../app.postcss";
-  import Sidebar from "$lib/components/Sidebar.svelte";
   import { page } from '$app/stores';
+  import Navbar from '$lib/components/Navbar.svelte';
 </script>
 
 <svelte:head>
   <title>RaspCard</title>
 </svelte:head>
 
-<main class="b-d">
-  {#if $page.url.pathname === '/login' || $page.status === 404 || $page.status === 500}
+<main class="w-screen h-screen">
+  {#if $page.data.currentAdmin}
+    <Navbar />
+    <slot />
+  {:else}
     <div class="h-full w-full flex justify-center items-center">
       <slot />
     </div>
-  {:else}
-    <Sidebar establishment={$page.data.currentAdmin.establishmentName} seller={$page.data.currentAdmin.name} activeURL={$page.url.pathname}>
-      <slot />
-    </Sidebar>
   {/if}
 </main>
