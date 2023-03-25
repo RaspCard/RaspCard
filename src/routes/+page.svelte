0007bb46f0c1f43dd5f20310e430906c126da67c
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-03-25 15:16:04 +0100

    update home with less text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    import { P, Heading, Card, Span } from 'flowbite-svelte';
+    import { P, Heading, Card } from 'flowbite-svelte';
+    import { Home } from "svelte-heros-v2";
     import { goto } from '$app/navigation';
     import Scanner from "$lib/components/Scanner.svelte";
     import type { PageData } from './$types';
@@ -8,12 +9,8 @@
 </script>
 
 <Scanner on:scan={e => goto(`/users/${e.detail.id}`)}/>
-<div class="m-4 flex flex-col items-start gap-4">
-    <Heading tag="h1">Benvenuto</Heading>
-    <P size="lg" italic space="wide" whitespace="preline">
-        Il Nuovo sistema RaspCard con l'uso di un <Span class="font-semibold">dispositivo RFID</Span> permette di accedere al sistema con un semplice tocco.<br/>
-        <Span class="font-semibold">Online</Span> e <Span class="font-semibold">centralizzato</Span> in un unico punto, RaspCard è molto più veloce e semplice da usare.
-    </P>
+<div class="p-4 flex flex-col items-start gap-4">
+    <Heading tag="h2">Benvenuto</Heading>
     <Card class="p-4">
         <div class="flex flex-col items-center">
             <Heading tag="h5" class="mb-2">Utenti Attivi</Heading>
@@ -21,5 +18,5 @@
                 <P size="lg" weight="semibold">{data.userLenght}</P>
             </div>
         </div>
-      </Card>
+    </Card>
 </div>
