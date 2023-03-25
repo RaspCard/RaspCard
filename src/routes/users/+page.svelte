e7223be9f598154a7c3b4e61798b80d4ed9861a3
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-03-25 15:16:11 +0100

    remove useless class

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -13,8 +13,8 @@
 </script>
 
 <Scanner on:scan={e => goto(`/users/${e.detail.id}`)}/>
-<div class="container-raspcard m-4">
-    <div class="mx-4 h-[5vh]">
+<div class="flex flex-col p-4">
+    <div class="px-4 h-[5vh]">
         <Input autocomplete="off" id="search" placeholder="Search" size="md" bind:value={searchInput}>
             <!-- WHY? -->
             <svg slot="left" aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
@@ -33,11 +33,3 @@
         </TableHeadUsers>
     </Table>
 </div>
-
-
-<style>
-    .container-raspcard {
-        display: flex;
-        flex-direction: column;
-    }
-</style>
