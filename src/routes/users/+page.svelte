b0d41d91feaa544cf011ab9bfe42772f8a0a92c8
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-04-13 23:17:22 +0200

    update table creation and usage

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
     import type { PageData } from './$types';
     import { goto } from '$app/navigation';
-    import { Table, Input } from 'flowbite-svelte';
-    import TableRowUser from '$lib/components/TableRowUser.svelte';
-    import TableHeadUsers from '$lib/components/TableHeadUsers.svelte';
+    import { Input } from 'flowbite-svelte';
+    import Table from '$lib/components/Table.svelte';
+    import TableHead from '$lib/components/TableHead.svelte';
+    import TableRow from '$lib/components/TableRow.svelte';
     import Scanner from "$lib/components/Scanner.svelte";
 
     export let data: PageData;
@@ -13,23 +14,20 @@
 </script>
 
 <Scanner on:scan={e => goto(`/users/${e.detail.id}`)}/>
-<div class="flex flex-col p-4">
-    <div class="px-4 h-[5vh]">
+<div class="flex flex-col py-4 px-8 w-full h-full">
+    <!-- TODO: Update search bar -->
+    <div class="h-[10%] min-h-[10%]">
         <Input autocomplete="off" id="search" placeholder="Search" size="md" bind:value={searchInput}>
-            <!-- WHY? -->
+            <!-- Can't use Icon component for some reason -->
             <svg slot="left" aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
         </Input>
     </div>
-    <Table
-        class="border-collapse table-auto w-full whitespace-nowrap relative" 
-        divClass="m-4 scrollbar-none overflow-x-auto overflow-y-auto rounded-lg shadow relative max-h-[80vh] lg:max-h-[90vh] z-0"
-        striped 
-        hoverable={true}
-    >
-        <TableHeadUsers>
-            {#each dynamicUsers as user (user.id)}
-                <TableRowUser  {...user}/>
-            {/each}
-        </TableHeadUsers>
+    <!-- TODO: Change table with custom component -->
+    <!-- Custom Table -->
+    <Table>
+        <TableHead />
+        {#each dynamicUsers as user (user.id)}
+            <TableRow {...user}/>
+        {/each}
     </Table>
 </div>
