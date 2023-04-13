c5d42d98d62bc444724b5eff8aa94540bf849455
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-04-13 17:40:00 +0200

    Create chart component

---
+++ src/lib/components/Chart.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { Chart, DoughnutController, ArcElement } from 'chart.js';
+
+    export let chartName: string;
+    export let data: any;
+
+    // Update register to make the component handle the registration
+    Chart.register(DoughnutController);
+    Chart.register(ArcElement);
+
+    onMount(() => {
+        let ctx = document.getElementById(chartName) as HTMLCanvasElement;
+        new Chart(ctx, data).render();
+    });
+</script>
+
+<div>
+    <canvas id={chartName}></canvas>
+</div>
