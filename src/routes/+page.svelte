9b60678d1cf74dc1c6e60b2f27f89a512205b402
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-04-24 18:27:50 +0200

    update card with more information and responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,9 @@
 
     let userChartSettings = {
         type: 'doughnut',
+        options: {
+            responsive: true
+        },
         data: {
             labels: [
                 'attivi',
@@ -29,14 +32,30 @@
 </script>
 
 <Scanner on:scan={e => goto(`/users/${e.detail.id}`)}/>
-<div class="p-4 flex flex-col items-start gap-4">
-    <Heading tag="h2">Benvenuto</Heading>
-    <Card class="p-4 rounded-md">
-        <div class="flex flex-row">
-            <div class="flex flex-col items-center">
-                <Heading tag="h5" class="mb-2">Utenti Registrati</Heading>
-                <Chart chartName={"users"} data={userChartSettings}/>
+<div class="p-4 flex flex-col sm:items-start items-center gap-4">
+    <Heading class="!text-gray-700" tag="h2">Bentornato {data.currentAdmin.name}</Heading>
+    <Card class="p-4 rounded-md w-full sm:w-auto">
+        <div class="flex flex-col items-center">
+            <Heading tag="h5" class="!text-gray-700 text-center">Utenti Registrati</Heading>
+            <div class="w-full flex flex-row items-center justify-center gap-5 sm:gap-0 sm:flex-col sm:items-center">
+                <Chart divClass="w-24 h-24 sm:w-40 sm:h-40" chartName={"users"} data={userChartSettings}/>
+                <div class="flex flex-row gap-2 text-base">
+                    <div class="flex flex-col">
+                        <div class="flex flex-row items-center gap-1">
+                            <div class="w-3 h-3 rounded-sm bg-green-700"></div>
+                            <span>Utenti Attivi</span>
+                        </div>
+                        <div class="flex flex-row items-center gap-1">
+                            <div class="w-3 h-3 rounded-sm bg-slate-400"></div>
+                            <span>Utenti Disattivati</span>
+                        </div>
+                    </div>
+                    <div class="flex flex-col font-extrabold">
+                        <span>{data.activeUsersCount}</span>
+                        <span>{data.inactiveUsersCount}</span>
+                    </div>
+                </div>
             </div>
-        </div>
+        <div/>
     </Card>
 </div>
