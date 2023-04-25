b54e4b3a1e50a68159d3c34146031bde15ae9112
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-04-25 23:00:56 +0200

    upload the skeleton for the new card latest transactions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,30 +32,43 @@
 </script>
 
 <Scanner on:scan={e => goto(`/users/${e.detail.id}`)}/>
-<div class="p-4 flex flex-col sm:items-start items-center gap-4">
+<div class="p-4 flex flex-col lg:items-start items-center gap-4">
     <Heading class="!text-gray-700" tag="h2">Bentornato {data.currentAdmin.name}</Heading>
-    <Card class="p-4 rounded-md w-full sm:w-auto">
-        <div class="flex flex-col items-center">
-            <Heading tag="h5" class="!text-gray-700 text-center">Utenti Registrati</Heading>
-            <div class="w-full flex flex-row items-center justify-center gap-5 sm:gap-0 sm:flex-col sm:items-center">
-                <Chart divClass="w-24 h-24 sm:w-40 sm:h-40" chartName={"users"} data={userChartSettings}/>
-                <div class="flex flex-row gap-2 text-base">
-                    <div class="flex flex-col">
-                        <div class="flex flex-row items-center gap-1">
-                            <div class="w-3 h-3 rounded-sm bg-green-700"></div>
-                            <span>Utenti Attivi</span>
-                        </div>
-                        <div class="flex flex-row items-center gap-1">
-                            <div class="w-3 h-3 rounded-sm bg-slate-400"></div>
-                            <span>Utenti Disattivati</span>
-                        </div>
+    <div class="flex flex-col lg:flex-row gap-2">
+        <Card class="p-4 rounded-md w-full lg:w-auto">
+            <div class="flex flex-col">
+                {#each data.latestTransactions as transaction}
+                    <div class="flex flex-row gap-4">
+                        <span>{transaction.createdAt.toLocaleTimeString()}</span>
+                        <span>{transaction.user.cardId}</span>
+                        <span>{transaction.transaction}</span>
                     </div>
-                    <div class="flex flex-col font-extrabold">
-                        <span>{data.activeUsersCount}</span>
-                        <span>{data.inactiveUsersCount}</span>
+                {/each}
+            </div>
+        </Card>
+        <Card class="p-4 rounded-md w-full lg:w-auto">
+            <div class="flex flex-col items-center">
+                <Heading tag="h5" class="!text-gray-700 text-center">Utenti Registrati</Heading>
+                <div class="w-full flex flex-row items-center justify-center gap-5 sm:gap-0 sm:flex-col sm:items-center">
+                    <Chart divClass="w-24 h-24 sm:w-40 sm:h-40" chartName={"users"} data={userChartSettings}/>
+                    <div class="flex flex-row gap-2 text-base">
+                        <div class="flex flex-col">
+                            <div class="flex flex-row items-center gap-1">
+                                <div class="w-3 h-3 rounded-sm bg-green-700"></div>
+                                <span>Utenti Attivi</span>
+                            </div>
+                            <div class="flex flex-row items-center gap-1">
+                                <div class="w-3 h-3 rounded-sm bg-slate-400"></div>
+                                <span>Utenti Disattivati</span>
+                            </div>
+                        </div>
+                        <div class="flex flex-col font-extrabold">
+                            <span>{data.activeUsersCount}</span>
+                            <span>{data.inactiveUsersCount}</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-        <div/>
-    </Card>
+            <div/>
+        </Card>
+    </div>
 </div>
