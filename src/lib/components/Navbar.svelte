e09611755e317e8b888a84efd9c41989f27edaf8
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-05-16 23:04:34 +0200

    update navbar to fix the position issue
    Change the style of the links for the other pages. This is caused by the usage of flex-wrap that was creating artifacts over the mobile view

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -8,39 +8,51 @@
 </script>
 
 <!-- TODO: add page selected -->
-<Navbar class="h-20 relative z-10 !text-primary" navDivClass="h-full max-w-full flex flex-row items-center justify-center" let:hidden let:toggle>
-    <div class="flex flex-wrap justify-between container">
-        <NavBrand href="/">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo"/>
-            <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Raspcard</span>
-        </NavBrand>
-        <div class="flex items-center md:order-2">
-            <A id="more-info" class="text-primary font-medium hover:no-underline hover:text-accent">ADMIN</A>
-            <NavHamburger on:click={toggle} class="w-full md:flex md:w-auto md:order-1" />
+<div class="relative">
+    <Navbar
+      navClass="px-2 sm:px-4 py-2.5 absolute w-full z-20 top-0 left-0 border-b h-20"
+      navDivClass="h-full mx-auto"
+    >
+        <div class="h-full flex flex-row items-center justify-between container">
+            <NavBrand href="/">
+                <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo"/>
+                <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Raspcard</span>
+            </NavBrand>
+            <div class="flex items-center gap-2 md:order-2">
+                <A id="more-info" class="text-primary font-medium hover:no-underline hover:text-accent">ADMIN</A>
+                <NavHamburger btnClass="lg:hidden" id="paths" />
+            </div>
+            <Dropdown placement="bottom" triggeredBy="#more-info">
+                <DropdownHeader>
+                    <span class="block text-sm font-bold">{ username }</span>
+                    <span class="block truncate text-sm font-medium">{ systemName }</span>
+                </DropdownHeader>
+                <DropdownItem href="/">Home</DropdownItem>
+                <DropdownDivider />
+                <form
+                    method="POST"
+                    action="/logout"
+                    use:enhance
+                >
+                    <button type="submit" class="w-full">
+                        <DropdownItem class="flex flex-row items-center gap-2"><ArrowLeftOnRectangle class="w-5 h-5"/>esci</DropdownItem>
+                    </button>
+                </form>
+            </Dropdown>
+            <Dropdown placement="bottom" triggeredBy="#paths">
+                <DropdownItem class="!text-primary hover:!text-accent" href="/">HOME</DropdownItem>
+                <DropdownItem class="!text-primary hover:!text-accent" href="/users">UTENTI</DropdownItem>
+                <DropdownItem class="!text-primary hover:!text-accent" href="/create">CREAZIONE UTENTI</DropdownItem>
+                <DropdownItem class="!text-primary hover:!text-accent" href="/tos">TERMINI E CONDIZIONI</DropdownItem>
+                <DropdownItem class="!text-primary hover:!text-accent" href="/help">AIUTO</DropdownItem>
+            </Dropdown>
+            <NavUl class="hidden lg:flex flex-row">
+                <NavLi class="!text-primary hover:!text-accent" href="/">HOME</NavLi>
+                <NavLi class="!text-primary hover:!text-accent" href="/users">UTENTI</NavLi>
+                <NavLi class="!text-primary hover:!text-accent" href="/create">CREAZIONE UTENTI</NavLi>
+                <NavLi class="!text-primary hover:!text-accent" href="/tos">TERMINI E CONDIZIONI</NavLi>
+                <NavLi class="!text-primary hover:!text-accent" href="/help">AIUTO</NavLi>
+            </NavUl>
         </div>
-        <Dropdown placement="bottom" triggeredBy="#more-info">
-            <DropdownHeader>
-                <span class="block text-sm font-bold">{ username }</span>
-                <span class="block truncate text-sm font-medium">{ systemName }</span>
-            </DropdownHeader>
-            <DropdownItem href="/">Home</DropdownItem>
-            <DropdownDivider />
-            <form
-                method="POST"
-                action="/logout"
-                use:enhance
-            >
-                <button type="submit" class="w-full">
-                    <DropdownItem class="flex flex-row items-center gap-2"><ArrowLeftOnRectangle class="w-5 h-5"/>esci</DropdownItem>
-                </button>
-            </form>
-        </Dropdown>
-        <NavUl {hidden}>
-            <NavLi class="!text-primary hover:!text-accent" href="/">HOME</NavLi>
-            <NavLi class="!text-primary hover:!text-accent" href="/users">UTENTI</NavLi>
-            <NavLi class="!text-primary hover:!text-accent" href="/create">CREAZIONE UTENTI</NavLi>
-            <NavLi class="!text-primary hover:!text-accent" href="/tos">TERMINI E CONDIZIONI</NavLi>
-            <NavLi class="!text-primary hover:!text-accent" href="/help">AIUTO</NavLi>
-        </NavUl>
-    </div>
-</Navbar>
+    </Navbar>
+</div>
