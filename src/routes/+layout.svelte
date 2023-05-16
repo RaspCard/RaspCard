ba027358706d0919b5193807d68d3643ed1dbe60
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-05-16 23:06:09 +0200

    add div for space of navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,6 +11,7 @@
 <main class="w-screen h-screen">
   {#if $page.data.currentAdmin}
     <Navbar username={$page.data.currentAdmin.name} systemName={$page.data.currentAdmin.establishmentName}/>
+    <div class="h-20"></div>
     <div class="w-full h-[calc(100svh-5rem)]">
       <slot />
     </div>
