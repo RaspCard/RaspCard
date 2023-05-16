a35793578d5aa80466b848459be42e144105a5bd
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-05-16 23:05:12 +0200

    update max-h for table display

--- src/lib/components/Table.svelte
+++ src/lib/components/Table.svelte
@@ -1,4 +1,4 @@
-<div class="w-full max-h-[90%] overflow-x-auto overflow-y-auto scrollbar-none border-x border-y rounded">
+<div class="w-full max-h-[calc(100%-3rem)] overflow-x-auto overflow-y-auto scrollbar-none border-x border-y rounded">
     <table class="min-w-[100%] text-base">
         <slot />
     </table>
