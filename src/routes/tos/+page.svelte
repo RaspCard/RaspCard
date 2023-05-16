1600747931db40294e27987c5719ff4cba4fab99
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-05-16 23:49:15 +0200

    create help and tos pages

---
+++ src/routes/tos/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { Heading, Span, P, List, Li } from "flowbite-svelte";
+</script>
+
+<div class="w-full flex justify-center max-h-full overflow-y-scroll">
+    <div class="container text-left">
+
+    </div>
+</div>
