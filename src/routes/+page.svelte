808b8677639fea60ea3cb61479e672d964df3f3c
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-05-16 18:07:57 +0200

    update graphics for card in base page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,6 @@
     import { goto } from '$app/navigation';
     import Scanner from "$lib/components/Scanner.svelte";
     import Chart from "$lib/components/Chart.svelte";
-    import type { PageData } from './$types';
 
     export let data: PageData;
 
@@ -21,8 +20,8 @@
                 label: 'UserChart',
                 data: [data.activeUsersCount, data.inactiveUsersCount],
                 backgroundColor: [
-                '#15803d',
-                '#94a3b8'
+                "#825f0d",
+                "#f2ecdb"
                 ],
                 circumference: 240,
                 rotation: 240
@@ -33,42 +32,45 @@
 
 <Scanner on:scan={e => goto(`/users/${e.detail.id}`)}/>
 <div class="p-4 flex flex-col lg:items-start items-center gap-4">
-    <Heading class="!text-gray-700" tag="h2">Bentornato {data.currentAdmin.name}</Heading>
+    <Heading class="!text-primary" tag="h2">Bentornato {data.currentAdmin.name}</Heading>
     <div class="flex flex-col lg:flex-row gap-2">
-        <Card class="p-4 rounded-md w-full lg:w-auto">
+        <Card class="p-4 w-full lg:w-auto border-none shadow-none relative">
+            <!-- TODO: update card with new table -->
             <div class="flex flex-col">
                 {#each data.latestTransactions as transaction}
-                    <div class="flex flex-row gap-4">
+                    <div class="flex flex-row gap-4 font-mono">
                         <span>{transaction.createdAt.toLocaleTimeString()}</span>
                         <span>{transaction.user.cardId}</span>
                         <span>{transaction.transaction}</span>
                     </div>
                 {/each}
             </div>
+            <div class="absolute w-full h-full flex-1 bg-primary opacity-5 top-0 left-0 rounded-md"></div>
         </Card>
-        <Card class="p-4 rounded-md w-full lg:w-auto">
+        <Card class="p-4 w-full lg:w-auto border-none shadow-none relative text-primary">
             <div class="flex flex-col items-center">
-                <Heading tag="h5" class="!text-gray-700 text-center">Utenti Registrati</Heading>
+                <Heading tag="h5" class="text-center">Utenti Registrati</Heading>
                 <div class="w-full flex flex-row items-center justify-center gap-5 sm:gap-0 sm:flex-col sm:items-center">
                     <Chart divClass="w-24 h-24 sm:w-40 sm:h-40" chartName={"users"} data={userChartSettings}/>
                     <div class="flex flex-row gap-2 text-base">
                         <div class="flex flex-col">
                             <div class="flex flex-row items-center gap-1">
-                                <div class="w-3 h-3 rounded-sm bg-green-700"></div>
+                                <div class="w-3 h-3 rounded-sm bg-accent"></div>
                                 <span>Utenti Attivi</span>
                             </div>
                             <div class="flex flex-row items-center gap-1">
-                                <div class="w-3 h-3 rounded-sm bg-slate-400"></div>
+                                <div class="w-3 h-3 rounded-sm bg-secondary-button"></div>
                                 <span>Utenti Disattivati</span>
                             </div>
                         </div>
-                        <div class="flex flex-col font-extrabold">
+                        <div class="flex flex-col font-semibold">
                             <span>{data.activeUsersCount}</span>
                             <span>{data.inactiveUsersCount}</span>
                         </div>
                     </div>
                 </div>
-            <div/>
+            </div>
+            <div class="absolute w-full h-full flex-1 bg-primary opacity-5 top-0 left-0 rounded-md"></div>
         </Card>
     </div>
 </div>
