9c62fcdc6381dd041ca4237f631f55b09d4924c5
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-05-16 23:49:04 +0200

    update error page layout and style

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,12 +3,12 @@
     import { page } from "$app/stores";
 </script>
 
-<div class="flex flex-col gap-2 max-w-[20rem]">
-    <div class="flex flex-row">
+<div class="w-full h-full flex items-center justify-center gap-2">
+    <div class="flex flex-col items-center max-w-xl gap-3">
         <P weight="thin" size="5xl" class="flex items-center mr-2">
             {$page.status}
         </P>
-        <P weight="thin" size="2xl" class="flex items-center border-l-[1px] border-l-gray-400 pl-2">
+        <P weight="thin" size="2xl" class="text-center">
             {#if $page.status === 404}
                 La pagina/utente che stai cercando non esiste!
             {:else if $page.status === 500}
@@ -17,6 +17,6 @@
                 C'è stato un errore.
             {/if}
         </P>
+        <Button href="/" class="bg-primary-button active:ring-0 hover:bg-primary-button hover:opacity-90">Torna alla Home</Button>
     </div>
-    <Button outline color="red" href="/">Torna indietro!</Button>
 </div>
