070c74187555235f0b857543e04ad50c56357b1c
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-05-17 11:43:13 +0200

    add some padding text area

--- src/routes/tos/+page.svelte
+++ src/routes/tos/+page.svelte
@@ -2,7 +2,7 @@
     import { Heading, Span, P, List, Li } from "flowbite-svelte";
 </script>
 
-<div class="w-full flex justify-center max-h-full overflow-y-scroll">
+<div class="py-6 w-full flex justify-center max-h-full overflow-y-scroll">
     <div class="container text-left">
 
     </div>
