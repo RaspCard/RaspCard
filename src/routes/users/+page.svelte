40c1d7d239868b11bb79d2f9cabb47525a4c2d73
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-05-17 09:00:59 +0200

    remove todo message and focus color on search bar

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -20,9 +20,8 @@
 
 <Scanner on:scan={e => goto(`/users/${e.detail.id}`)}/>
 <div class="flex flex-col py-4 px-8 w-full h-full">
-    <!-- TODO: Update search bar -->
     <div class="h-12">
-        <Input autocomplete="off" id="search" placeholder="Cerca" size="md" bind:value={searchInput}>
+        <Input autocomplete="off" id="search" placeholder="Cerca" size="md" bind:value={searchInput} class="focus:border-secondary-button focus:ring-0">
             <!-- Can't use Icon component for some reason -->
             <svg slot="left" aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
         </Input>
