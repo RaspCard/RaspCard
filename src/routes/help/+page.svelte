b7471084e93f185f721e86260c85648b95e0ca32
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-05-17 09:00:27 +0200

    update help page with WIP message

---
+++ src/routes/help/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { Heading } from "flowbite-svelte";
+</script>
+
+<div class="w-full p-5">
+    <Heading tag="h2" class="text-center">Work In Progress</Heading>
+</div>
