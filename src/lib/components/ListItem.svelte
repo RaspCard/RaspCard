120cb92d85ff5945d2c18e6061e388aa5927a886
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-05-18 23:28:43 +0200

    update ListItem component
    Added new features:
    vertical flag
    badge flag
    settable variable for vertical view
    vertical view

--- src/lib/components/ListItem.svelte
+++ src/lib/components/ListItem.svelte
@@ -1,29 +1,42 @@
 <script lang="ts">
     import { Badge } from "flowbite-svelte";
 
+    export let vertical: boolean = false;
+    export let badge: boolean = false;
     export let fieldName: string;
     export let fieldValue: any;
     export let currency: string | undefined = "";
-    export let border: boolean = false;
+    export let width: string = "w-fit";
 
     function isNumber(value: any) {
         return typeof value === "number";
     }
 </script>
 
-
-<li class="w-full text-sm font-medium" class:border-t-2={border}>
+{#if vertical}
+<li class={"flex flex-col gap-1 font-medium " + width} >
+    <span class="text-lg font-semibold">Transazione</span>
+    {#if badge && fieldValue < 0}
+        <Badge class="!bg-secondary-button !text-accent" large={true}>PAGAMENTO</Badge>
+    {:else if badge && fieldValue > 0}
+        <Badge class="!bg-primary-button !text-secondary" large={true}>DEPOSITO</Badge>
+    {/if}
+    <span>{`${fieldValue}${currency !== undefined ? currency : ""}`}</span>
+</li>
+{:else}
+<li class="w-full text-sm font-medium">
     <div class="flex items-center space-x-4">
         <div class="flex-1 space-y-1 font-medium text-primary">
-            <div class="text-base font-bold">{fieldName}</div>
-            <div class="text-sm">
+            <span class="text-base font-bold">{fieldName}</span>
+            <div class="text-sm flex flex-row gap-2">
                 {`${fieldValue}${currency !== undefined ? currency : ""}`}
-                {#if isNumber(fieldValue) && fieldValue < 0}
-                    <Badge class="ml-2 !bg-secondary-button !text-accent" large={true}>PAGAMENTO</Badge>
-                {:else if isNumber(fieldValue) && fieldValue > 0}
-                    <Badge class="ml-2 !bg-primary-button !text-secondary" large={true}>DEPOSITO</Badge>
+                {#if badge && fieldValue < 0}
+                    <Badge class="!bg-secondary-button !text-accent" large={true}>PAGAMENTO</Badge>
+                {:else if badge && fieldValue > 0}
+                    <Badge class="!bg-primary-button !text-secondary" large={true}>DEPOSITO</Badge>
                 {/if}
             </div>
         </div>
     </div>
 </li>
+{/if}
